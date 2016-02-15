49c8b9908bfe92a240cceaa07535a9b864e59e96
Maes95 <michel.maes95@gmail.com>
2016-02-15 12:13:28 +0100

    Version-1.2

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -1,4 +1,4 @@
-var state = true; // true -> Cards, false -> List
+
 
 function cuenta(){
   var s = $('#report_content').val();
@@ -19,54 +19,3 @@ function cuenta(){
     }
   }
 }
-
-function cardView() {
-  state = true;
-  $( "#ReportTable" ).hide( "slow", function() {
-    $("#cards").children(".scene3D").each(function (){
-      $(this).show("medium");
-    });
-  });
-}
-
-function listView() {
-  state = false;
-  $("#cards").children(".scene3D").each(function (){
-    $(this).hide("slow");
-  });
-
-  setTimeout(
-  function()
-  {
-    $( "#ReportTable" ).show("slow");
-    $('#ReportTable tbody tr').each(function (){
-        $(this).show();
-    });
-  }, "slow");
-
-}
-
-function reportFilter(label){
-  if(state){ // Cardview
-    $("#cards").children(".scene3D").each(function (){
-      if(!$(this).hasClass(label+'-s')){
-        $(this).hide("slow");
-      }else{
-        $(this).show("slow");
-      }
-    });
-  }else{ // ListView
-    $('#ReportTable tbody tr').each(function (){
-      if(!$(this).hasClass(label+'-s')){
-        $(this).hide("slow");
-      }else{
-        $(this).show("slow");
-      }
-    });
-  }
-}
-
-
-$(document).ready(function(){
-  $( "#ReportTable" ).hide();
-});

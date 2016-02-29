f5a740e2bf3e37c2b2dbabf5f00473aff72efe5c
Maes95 <michel.maes95@gmail.com>
2016-02-29 11:36:12 +0100

    Mailer Production Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,13 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: "michel.maes95@gmail.com",
-      password: "Inefable08"
-
-  }
+  config.action_mailer.default_url_options = { :host => 'reportbeforeblame.herokuapp.com' }
 end
